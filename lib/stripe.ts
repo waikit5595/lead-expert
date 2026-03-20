9e9c1d44667f805aa3a262969a3e3dda17d2e6f6
waikit5595 <waikitwong5595@gmail.com>
2026-03-20 17:18:47 +0800

    fix stripe sdk version typing

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,6 +4,4 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('Missing STRIPE_SECRET_KEY');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-06-20',
-});
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
